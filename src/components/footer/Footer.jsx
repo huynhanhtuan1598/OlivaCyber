5b29977b1549c87ba5ab9f3bc17af85621f94fde
Nhựt Tân <119086524+Nguyennhuttan2004@users.noreply.github.com>
2024-09-27 11:26:56 +0700

    update footer header

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,92 @@
-import React from "react";
+import { Image } from 'antd'
+import Link from 'antd/es/typography/Link'
+import React from 'react'
 
-export default function footer() {
-  return <div />;
+const Footer = () => {
+  return (
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-[#181D38] text-sm mt-24">
+      {/* top */}
+      <div className="flex flex-col md:flex-row justify-between gap-24">
+        {/* left */}
+        <div className="w-full md:w-1/2  lg:w-1/4 flex flex-col gap-8">
+          <a href="/">
+            <div className="text-2xl tracking-wide font-bold text-white">Quick Link</div>
+          </a>
+          <a href="/" className='hover:text-base'>
+            <div className=" tracking-wide font-bold text-white">About Us</div>
+          </a>
+          <a href="/" className='hover:text-base'>
+            <div className=" tracking-wide font-bold text-white">Contact Us</div>
+          </a>
+          <a href="/" className='hover:text-base'>
+            <div className=" tracking-wide font-bold text-white ">Privacy Policy</div>
+          </a>
+          <a href="/" className='hover:text-base'>
+            <div className=" tracking-wide font-bold text-white">Term & Condition</div>
+          </a>
+          <a href="/" className='hover:text-base'>
+            <div className=" tracking-wide font-bold text-white">FAQs & Help</div>
+          </a>
+        </div>
+        {/* center */}
+        <div className="w-full md:w-1/2  lg:w-1/4 flex flex-col gap-6 ">
+        <h1 className="font-bold text-2xl text-white">Contact</h1>
+          <p className='font-bold text-white'>
+           123 Street, New York, USA
+          </p>
+          <p className='font-bold text-white'>+01234567890</p>
+          <p className='font-bold text-white  '> info@gmail.com</p> 
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <h4 className='text-white mb-3 font-bold text-2xl'>Gallery</h4>
+          <div className="row g-2 pt-2">
+            <div className="col-4">
+              <Image src="./course-1.jpg" alt="" />
+            </div>
+            <div className="col-4"></div>
+            <div className="col-4"></div>
+            <div className="col-4"></div>
+            <div className="col-4"></div>
+            <div className="col-4"></div>
+          </div>
+        </div>
+        {/* right */}
+        <div className="w-full md:w-1/2  lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p>
+            Be the first to get the latest news about trends promotions and much
+            more!
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email address"
+              className="p-4 w-3/4"
+            />
+            <button className="w-1/4 bg-[#F35C7A] text-white">JOIN</button>
+          </div>
+          <span className="font-semibold">Secure Payments</span>
+          <div className="flex justify-between">
+           
+          </div>
+        </div>
+      </div>
+      {/* bottom */}
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="font-medium">@ 2024 SuMin Store</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">Viet Nam | TP Ho Chi Minh</span>
+          </div>
+          <div className="">
+          <span className="text-gray-500 mr-4">Currency</span>
+          <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Footer
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,98 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dropdown, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
-export default function components() {
-  return <div />;
-}
+import React from "react";
+import Link from "antd/es/typography/Link";
+
+const items = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/"
+      >
+        Our Team
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        Testimonial
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/"
+      >
+        404 Pages
+      </a>
+    ),
+   
+  },
+  
+];
+
+const Header = () => {
+  return (
+    <header className="">
+      <div className="container">
+        <nav className="bg-white border-spacing-8 flex flex-wrap items-center justify-between">
+          <a
+            href="/"
+            className="navbar-brand flex align-items-center px-4 px-lg-5"
+          >
+            <h2 class="ml-4  text-[#06bbcc] text-2xl font-bold">
+              <FontAwesomeIcon icon="fa-solid fa-book" />
+              eLEARNING
+            </h2>
+          </a>
+          <div className=" flex flex-wrap items-center justify-between ">
+            <div className=" space-x-5 mr-6 font-semibold">
+              <a href="/" className="text-[#06bbcc]">HOME</a>
+              <a href="/" className="hover:text-[#06bbcc]">ABOUT</a>
+              <a href="/" className="hover:text-[#06bbcc]">COURSES</a>
+              <Dropdown
+                menu={{
+                  items,
+                }}
+                arrow={true}
+                trigger={["click"]}
+                className="cursor-pointer py-3 px-4 hover:bg-gray-100 duration-300 rounded-md hover:text-[#06bbcc]"
+              >
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    PAGES
+                    <DownOutlined />
+                  </Space>
+                </a>
+              </Dropdown>
+              <a href="/" className="hover:text-[#06bbcc]">CONTACT</a>
+            </div>
+            <a
+              href="/"
+              className="py-6 px-10 bg-[#06bbcc] font-medium text-white"
+            >
+              Join now
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
